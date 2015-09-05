7d0e6ddbfdb7abd6cc1060f672901a4288ee09a0
Marc Scheib <marc.scheib@gmx.de>
2015-09-05 19:23:43 +0200

    #1: fixed maven compiling
    Added a servlet to the web project to compile via maven. In addition, added beans.xml to allow injecting loggers.

--- project/cycribase-web/src/main/java/com/cycrilab/cycribase/TestServlet.java
+++ project/cycribase-web/src/main/java/com/cycrilab/cycribase/TestServlet.java
@@ -3,13 +3,9 @@ package com.cycrilab.cycribase;
 import org.slf4j.Logger;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 
 import javax.inject.Inject;
-import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,33 +18,11 @@ public class TestServlet extends HttpServlet {
     @Inject
     protected Logger logger;
 
-    private String id = "1";
-
-    @Override
-    public void service(ServletRequest req, ServletResponse res)
-            throws ServletException, IOException {
-        if (req instanceof HttpServletRequest) {
-            Field[] fields = this.getClass().getDeclaredFields();
-            for (Field f : fields) {
-                if (!f.getName().equals("id")) continue;
-                this.logger.info(f.getName());
-                try {
-                    this.logger.info(req.getParameterMap().get(f.getName())[0]);
-                    f.set(this, req.getParameterMap().get(f.getName())[0]);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-
-        super.service(req, res);
-    }
-
     @Override
     protected final void doGet(HttpServletRequest request, HttpServletResponse response) {
         try (ServletOutputStream outputStream = response.getOutputStream()) {
-            outputStream.write(this.id.getBytes("UTF-8"));
+            this.logger.info("Oha");
+            outputStream.print("Hello World");
         } catch (IOException e) {
             e.printStackTrace();
         }
