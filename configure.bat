bc3c4355a108290249b3c00cfec5f0e149066e6a
Marc Scheib <marc.scheib@gmx.de>
2015-09-05 17:07:43 +0200

    #1: initial project setup and wildfly configurator
    Created initial project as ear deployment (including ejb and war). Added a configuration package to configure Wildfly standalone.xml

---
+++ configure.bat
@@ -0,0 +1,2 @@
+cd config
+mvn process-resources wildfly:execute-commands -P "%1"
---
+++ project/cycribase-ejb/src/main/java/com/cycrilab/cycribase/logger/Loggers.java
@@ -0,0 +1,16 @@
+package com.cycrilab.cycribase.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+@Dependent
+public class Loggers {
+    @Produces
+    public Logger getLogger(InjectionPoint injectionPoint) {
+        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getSimpleName());
+    }
+}
